6b2e439cd01d9ab7815643f873e8d1b8d6b626f1
GuzyMateusz <maeusz521@gmail.com>
2024-04-24 14:11:42 +0200

    Update

---
+++ 1/Kolos1.cpp
@@ -0,0 +1,113 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#pragma warning(disable:4996)
+
+// Struktura zawierająca dwie zmienne: int i double
+struct PrzykladowaStruktura {
+    int calkowita;
+    double zmiennoprzecinkowa;
+};
+
+// Funkcja do alokacji tablicy instancji struktury
+PrzykladowaStruktura* alokujTablice(int rozmiar, const PrzykladowaStruktura& wzorzec) {
+    // Alokacja pamięci dla tablicy
+    PrzykladowaStruktura* tablica = new PrzykladowaStruktura[rozmiar];
+
+    // Inicjalizacja tablicy przy użyciu instancji wzorcowej
+    for (int i = 0; i < rozmiar; ++i) {
+        tablica[i] = wzorzec;
+    }
+
+    // Zwrócenie wskaźnika na utworzoną tablicę
+    return tablica;
+}
+
+// Funkcja do zapisu tablicy do pliku w trybie tekstowym
+void zapiszTabliceDoPliku(const char* nazwaPliku, const PrzykladowaStruktura* tablica, int rozmiar) {
+    // Otwarcie pliku do zapisu
+    std::ofstream plik(nazwaPliku);
+    if (!plik.is_open()) {
+        std::cerr << "Nie udalo sie otworzyc pliku do zapisu." << std::endl;
+        return;
+    }
+
+    // Zapisanie rozmiaru tablicy do pliku
+    plik << rozmiar << std::endl;
+
+    // Zapisanie zawartości tablicy do pliku
+    for (int i = 0; i < rozmiar; ++i) {
+        plik << tablica[i].calkowita << " " << tablica[i].zmiennoprzecinkowa << std::endl;
+    }
+
+    // Zamknięcie pliku
+    plik.close();
+}
+
+// Funkcja do odczytu tablicy z pliku w trybie tekstowym
+PrzykladowaStruktura* odczytajTabliceZPliku(const char* nazwaPliku, int& rozmiar) {
+    // Otwarcie pliku do odczytu
+    std::ifstream plik(nazwaPliku);
+    if (!plik.is_open()) {
+        std::cerr << "Nie udalo sie otworzyc pliku do odczytu." << std::endl;
+        return nullptr;
+    }
+
+    // Odczytanie rozmiaru tablicy z pliku
+    plik >> rozmiar;
+
+    // Alokacja pamięci dla tablicy na podstawie odczytanego rozmiaru
+    PrzykladowaStruktura* tablica = new PrzykladowaStruktura[rozmiar];
+
+    // Odczytanie zawartości tablicy z pliku
+    for (int i = 0; i < rozmiar; ++i) {
+        plik >> tablica[i].calkowita >> tablica[i].zmiennoprzecinkowa;
+    }
+
+    // Zamknięcie pliku
+    plik.close();
+
+    // Zwrócenie wskaźnika do wczytanej tablicy
+    return tablica;
+}
+
+
+
+int main() {
+    // Kod ASCII litery 'Z'
+    int rozmiar = 90;
+
+    // Przykładowa instancja wzorcowa
+    PrzykladowaStruktura wzorzec;
+    wzorzec.calkowita = 10;
+    wzorzec.zmiennoprzecinkowa = 3.14;
+
+    // Użycie funkcji do alokacji tablicy
+    PrzykladowaStruktura* tablica = alokujTablice(rozmiar, wzorzec);
+
+    // Zapis tablicy do pliku
+    zapiszTabliceDoPliku("tablica.txt", tablica, rozmiar);
+
+
+    // Sprawdzenie, czy tablice są identyczne
+    bool identyczne = true;
+    for (int i = 0; i < rozmiar; ++i) {
+        if (tablica[i].calkowita != wzorzec.calkowita || tablica[i].zmiennoprzecinkowa != wzorzec.zmiennoprzecinkowa) {
+			identyczne = false;
+			break;
+		}
+	}
+
+    if (identyczne) {
+        std::cout << "Tablice sa identyczne." << std::endl;
+    }
+    else {
+        std::cout << "Tablice nie sa identyczne." << std::endl;
+    }
+
+    // Zwolnienie zaalokowanej pamięci
+    delete[] tablica;
+
+    return 0;
+}
---
+++ 2/Kolos2.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+
+
+// Struktura zawierająca dwie zmienne: int i double
+struct Typ {
+	int calkowita;
+	double zmiennoprzecinkowa;
+};
+
+// Funkcja do alokacji tablicy instancji struktury
+Typ* alokujTablice(int rozmiar, const Typ& wzorzec) {
+	// Alokacja pamięci dla tablicy
+	Typ* tablica = new Typ[rozmiar];
+
+	// Inicjalizacja tablicy przy użyciu instancji wzorcowej
+	for (int i = 0; i < rozmiar; ++i) {
+		tablica[i] = wzorzec;
+	}
+
+	// Zwrócenie wskaźnika na utworzoną tablicę
+	return tablica;
+}
+
+
+void zapiszPlik(const char* nazwaPliku, Typ* tablica, int rozmiar) {
+	// Otwarcie pliku do zapisu
+	ofstream plik(nazwaPliku);
+
+	// Zapisanie wymiarów tablicy
+	plik << rozmiar << endl;
+
+	// Zapisanie zawartości tablicy
+	for (int i = 0; i < rozmiar; ++i) {
+		plik << tablica[i].calkowita << " " << tablica[i].zmiennoprzecinkowa << endl;
+	}
+
+	// Zamknięcie pliku
+	plik.close();
+}
+
+
+Typ* odczytajPlik(const char* nazwaPliku, int& rozmiar) {
+	// Otwarcie pliku do odczytu
+	ifstream plik(nazwaPliku);
+
+	// Odczytanie wymiarów tablicy
+	plik >> rozmiar;
+
+	// Alokacja pamięci dla tablicy
+	Typ* tablica = new Typ[rozmiar];
+
+	// Odczytanie zawartości tablicy
+	for (int i = 0; i < rozmiar; ++i) {
+		plik >> tablica[i].calkowita >> tablica[i].zmiennoprzecinkowa;
+	}
+
+	// Zamknięcie pliku
+	plik.close();
+
+	// Zwrócenie wskaźnika na utworzoną tablicę
+	return tablica;
+}
+
+int main()
+{
+   
+	Typ wzorzec = { 1, 2.5 };
+	int rozmiar = 71;
+
+	Typ* tablica = alokujTablice(rozmiar, wzorzec);
+	zapiszPlik("plik.txt", tablica, rozmiar);
+
+	Typ* odczytanaTablica = odczytajPlik("plik.txt", rozmiar);
+	
+
+	bool identyczne = true;
+	for (int i = 0; i < rozmiar; ++i) {
+		if (tablica[i].calkowita != odczytanaTablica[i].calkowita ||
+			tablica[i].zmiennoprzecinkowa != odczytanaTablica[i].zmiennoprzecinkowa) {
+			identyczne = false;
+			break;
+		}
+	}
+
+	if (identyczne) {
+		cout << "Tablice są identyczne" << endl;
+	}
+	else {
+		cout << "Tablice nie są identyczne" << endl;
+	}
+	
+
+	delete[] tablica;
+	delete[] odczytanaTablica;
+
+	return 0;
+}
+
